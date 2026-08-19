81d6879fd0a8de0140ab9173bed0eb43c0a74730
garryk3 <egoruum@gmail.com>
2026-08-19 16:01:51 +0300

    task/15.34: vector drawable для иконки уведомлений + manifestPlaceholders

--- fitnessappai/android/app/build.gradle.kts
+++ fitnessappai/android/app/build.gradle.kts
@@ -23,6 +23,8 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        manifestPlaceholders["default_notification_icon"] = "@drawable/ic_stat_launcher"
     }
 
     buildTypes {
